1ea0164e332d6878455ac7c943cf8dbfe4eb2aa7
Rachel Adler <adler.rachel.a@gmail.com>
2016-07-18 16:57:31 -0400

    fixed merge conflicts, again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0d2dc6e7e03faf6f52ea8b9c99ac818b1d99c447
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -58,38 +54,3 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-<<<<<<< HEAD
-=======
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-
-ActiveRecord::Migration.maintain_test_schema!
-
-RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.use_transactional_fixtures = true
-
-  config.infer_spec_type_from_file_location!
-
-  config.filter_rails_from_backtrace!
-
-  config.include FactoryGirl::Syntax::Methods
-  FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
-
-  config.include Warden::Test::Helpers
-  config.before :suite do
-    Warden.test_mode!
-  end
-
-  config.after :each do
-    Warden.test_reset!
-  end
->>>>>>> 95188231788ae019780a5519a1664ef1291c95a8
-=======
->>>>>>> 0d2dc6e7e03faf6f52ea8b9c99ac818b1d99c447
-end
