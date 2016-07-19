235905ebb1a33c573f8565a8d49afbb3ee7a359f
Rachel Adler <adler.rachel.a@gmail.com>
2016-07-18 21:47:31 -0400

    fixing bugs created by merge fixes

--- spec/models/ink_spec.rb
+++ spec/models/ink_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe Ink do
-  it { should have_valid(:color_name).when("Benevolent Badger Blue") }
+  it { should have_valid(:color_name).when('Benevolent Badger Blue') }
   it { should_not have_valid(:color_name).when(nil, '') }
 
   it { should have_valid(:manufacturer).when("Noodler's") }
 
-  it { should have_valid(:notes).when("I love this ink so much! I use it for everything.") }
+  it { should have_valid(:notes).when('I love this ink so much! I use it for everything.') }
 end
