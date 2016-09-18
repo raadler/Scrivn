167fdbafb0177791b543a8601a8220265a975c38
Rachel Adler <adler.rachel.a@gmail.com>
2016-09-18 19:50:22 -0400

    add nib model, schema, model tests

---
+++ app/models/nib.rb
@@ -0,0 +1,3 @@
+class Nib < ActiveRecord::Base
+  validates :nib_type, presence: true
+end
---
+++ spec/models/nib_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Nib do
+  it { should have_valid(:nib_size).when('medium') }
+
+  it { should have_valid(:nib_type).when('oblique') }
+  it { should_not have_valid(:nib_type).when(nil, '')}
+end
