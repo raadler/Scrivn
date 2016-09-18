ccb7c762aaa1f3080d59c303019eac621f2386fd
Rachel Adler <adler.rachel.a@gmail.com>
2016-09-18 19:55:22 -0400

    fix hound violation

--- spec/models/nib_spec.rb
+++ spec/models/nib_spec.rb
@@ -4,5 +4,5 @@ describe Nib do
   it { should have_valid(:nib_size).when('medium') }
 
   it { should have_valid(:nib_type).when('oblique') }
-  it { should_not have_valid(:nib_type).when(nil, '')}
+  it { should_not have_valid(:nib_type).when(nil, '') }
 end
